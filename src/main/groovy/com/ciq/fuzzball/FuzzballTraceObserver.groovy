75d424cb5a0eb7541d08a2fb72b7698167c53dea
Wolfgang Resch <wresch@ciq.com>
2025-05-30 15:41:47 -0400

    Create a yaml dumper specialized for fuzzball models

--- src/main/groovy/com/ciq/fuzzball/FuzzballTraceObserver.groovy
+++ src/main/groovy/com/ciq/fuzzball/FuzzballTraceObserver.groovy
@@ -1,8 +1,7 @@
 package com.ciq.fuzzball
 
+import java.nio.file.Path
 
-import com.ciq.fuzzball.model.WorkflowDefinition
-import com.ciq.fuzzball.model.WorkflowDefinitionJob
 import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
 import nextflow.Session
@@ -12,10 +11,9 @@ import nextflow.processor.TaskRun
 import nextflow.trace.TraceObserver
 import nextflow.trace.TraceRecord
 
-import org.yaml.snakeyaml.Yaml
-import org.yaml.snakeyaml.DumperOptions
-
-import java.nio.file.Path
+import com.ciq.fuzzball.model.WorkflowDefinition
+import com.ciq.fuzzball.model.WorkflowDefinitionJob
+import com.ciq.fuzzball.FuzzballYamlDumper
 
 /**
  * Implements an observer that allows implementing custom
@@ -24,6 +22,8 @@ import java.nio.file.Path
 @Slf4j
 @CompileStatic
 class FuzzballTraceObserver implements TraceObserver {
+
+    static final FuzzballYamlDumper dumper = new FuzzballYamlDumper()
 
     @Override
     void onFlowCreate(Session session) {
@@ -51,31 +51,21 @@ class FuzzballTraceObserver implements TraceObserver {
 
     @Override
     void onProcessSubmit(TaskHandler handler, TraceRecord trace) {
-        println "Process submitted!"
+        println "Process submitted! Will use container ${handler.task.getContainer()}"
+        // TODO: this is only temporary for testing purposes
+        TaskRun task = handler.getTask()
+        WorkflowDefinitionJob job = FuzzballWorkflowDefinitionJobFactory.create(task)
+        WorkflowDefinition wfDef = new WorkflowDefinition(
+            version: "v1",
+            jobs: [(job.getName()): job]
+        )
+        println("Workflow Definition:")
+        println(dumper.dump(wfDef))
     }
 
     @Override
     void onProcessStart(TaskHandler handler, TraceRecord trace) {
         println "Process started!"
-        TaskRun task = handler.getTask()
-
-        // Generate a Fuzzball job using a task
-        FuzzballWorkflowDefinitionJobFactory jobFactory = new FuzzballWorkflowDefinitionJobFactory()
-        WorkflowDefinitionJob job = jobFactory.create(task)
-        
-        // Add the job into a WorkflowDefinition
-        WorkflowDefinition wfDef = new WorkflowDefinition(
-            version: "v1",
-            jobs: [(job.getName()): job]
-        )
-
-        // Print the workflow definition in YAML format
-        DumperOptions options = new DumperOptions()
-        options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK)
-        options.setPrettyFlow(true)
-        Yaml yaml = new Yaml(options)
-        String yamlStr = yaml.dump(wfDef)
-        println "Workflow Definition YAML:\n${yamlStr}"
     }
 
     @Override

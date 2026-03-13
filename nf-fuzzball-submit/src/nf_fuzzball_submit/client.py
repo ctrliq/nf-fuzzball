2e8a5f6c97d833bcf508320e338b2ea05f57e345
Wolfgang Resch <wresch@ciq.com>
2026-03-12 22:01:29 -0400

    Makes depends-on a list of maps

--- nf-fuzzball-submit/src/nf_fuzzball_submit/client.py
+++ nf-fuzzball-submit/src/nf_fuzzball_submit/client.py
@@ -418,7 +418,7 @@ class FuzzballClient:
                         "env": env + ([f"FB_CA_CERT={ca_cert_path}"] if self._ca_cert_file is not None else []),
                         "policy": {"timeout": {"execute": args.timelimit}},
                         "resource": {"cpu": {"cores": args.cores}, "memory": {"size": args.memory}},
-                        "depends-on": {"name": "setup", "status": "FINISHED"},
+                        "depends-on": [{"name": "setup", "status": "FINISHED"}],
                     },
                 },
             },
